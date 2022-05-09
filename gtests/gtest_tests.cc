933e08e92763fdf5aa1025be37d7407520bc0152
Michał Wichrowski <mtwichrowski@gmail.com>
2022-05-09 16:47:29 +0200

    remove not working test

--- gtests/gtest_tests.cc
+++ gtests/gtest_tests.cc
@@ -6,12 +6,3 @@ TEST(AsIlike, whatever)
   ++i;
   EXPECT_EQ((unsigned int)2, i);
 }
-
-
-//FIXME!!!
-TEST(AsIlike, DISABLED_failure)
-{
-  unsigned int i = 1;
-  ++i;
-  EXPECT_EQ((unsigned int)3, i);
-}
